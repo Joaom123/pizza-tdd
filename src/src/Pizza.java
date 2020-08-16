4b8ddedf306e514ff38b0e5dbb8db5ccd1f166db
Joaom123 <joaomarcus12769@gmail.com>
2020-08-16 19:59:23 -0300

    implementando e testando adiciona ingrediente e getPreco

--- src/src/Pizza.java
+++ src/src/Pizza.java
@@ -1,7 +1,24 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pizza {
+    public List<String> ingredientes;
+
+    public Pizza() {
+        ingredientes = new ArrayList<String>();
+    }
 
     public void adicionaIngrediente(String ingrediente) {
+        ingredientes.add(ingrediente);
+    }
+
+    public int getPreco() {
+        int quantidadeDeIngredientes = ingredientes.size();
+
+        if(quantidadeDeIngredientes < 3)
+            return 15;
+        return ingredientes.size();
     }
 }
--- src/test/PizzaTest.java
+++ src/test/PizzaTest.java
@@ -1,7 +1,12 @@
 package test;
 
-import org.junit.Test;
 import src.Pizza;
+import org.junit.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class PizzaTest {
 
@@ -9,5 +14,21 @@ public class PizzaTest {
     public void adicionaIngrediente() {
         Pizza pizza = new Pizza();
         pizza.adicionaIngrediente("queijo");
+
+        ArrayList<String> ingredientes = new ArrayList<String>();
+        ingredientes.add("queijo");
+
+        assertEquals(ingredientes, pizza.ingredientes);
     }
+
+    @Test
+    public void precoAte2Ingredientes() {
+        Pizza pizza = new Pizza();
+        pizza.adicionaIngrediente("queijo");
+
+        int precoDaPizza = 15;
+
+        assertEquals(precoDaPizza, pizza.getPreco());
+    }
+
 }
