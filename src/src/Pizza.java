c8a37d988ce531665e9cc5fbc48f33de4af87872
Joaom123 <joaomarcus12769@gmail.com>
2020-08-16 19:17:17 -0300

    configurando junit | primeiro teste

---
+++ src/src/Pizza.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Pizza {
+
+    public void adicionaIngrediente(String ingrediente) {
+    }
+}
--- src/test/PizzaTest.java
+++ src/test/PizzaTest.java
@@ -1,4 +1,13 @@
 package test;
 
+import org.junit.Test;
+import src.Pizza;
+
 public class PizzaTest {
+
+    @Test
+    public void adicionaIngrediente() {
+        Pizza pizza = new Pizza();
+        pizza.adicionaIngrediente("queijo");
+    }
 }
