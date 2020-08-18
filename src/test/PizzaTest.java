3cdbd935fdb3dce003b75bcb8cad45aeb9324b0b
Joaom123 <joaomarcus12769@gmail.com>
2020-08-18 01:02:05 -0300

    teste preco da pizza e contabilizacao de ingredientes

--- src/test/PizzaTest.java
+++ src/test/PizzaTest.java
@@ -1,12 +1,9 @@
 package test;
 
 import src.Pizza;
-import org.junit.*;
-
 import java.util.ArrayList;
-
+import org.junit.*;
 import static org.junit.Assert.assertEquals;
-
 
 public class PizzaTest {
 
@@ -15,14 +12,14 @@ public class PizzaTest {
         Pizza pizza = new Pizza();
         pizza.adicionaIngrediente("queijo");
 
-        ArrayList<String> ingredientes = new ArrayList<String>();
+        ArrayList<String> ingredientes = new ArrayList<>();
         ingredientes.add("queijo");
 
         assertEquals(ingredientes, pizza.ingredientes);
     }
 
     @Test
-    public void precoAte2Ingredientes() {
+    public void precoDaPizzaComUmIngredientes() {
         Pizza pizza = new Pizza();
         pizza.adicionaIngrediente("queijo");
 
@@ -31,4 +28,83 @@ public class PizzaTest {
         assertEquals(precoDaPizza, pizza.getPreco());
     }
 
+    @Test
+    public void precoDaPizzaComDoisIngredientes() {
+        Pizza pizza = new Pizza();
+        pizza.adicionaIngrediente("queijo");
+        pizza.adicionaIngrediente("presunto");
+
+        int precoDaPizza = 15;
+
+        assertEquals(precoDaPizza, pizza.getPreco());
+    }
+
+    @Test
+    public void precoDaPizzaComTresIngredientes() {
+        Pizza pizza = new Pizza();
+        pizza.adicionaIngrediente("queijo");
+        pizza.adicionaIngrediente("presunto");
+        pizza.adicionaIngrediente("alho");
+
+        int precoDaPizza = 20;
+
+        assertEquals(precoDaPizza, pizza.getPreco());
+    }
+
+    @Test
+    public void precoDaPizzaComQuatroIngredientes() {
+        Pizza pizza = new Pizza();
+        pizza.adicionaIngrediente("queijo");
+        pizza.adicionaIngrediente("presunto");
+        pizza.adicionaIngrediente("alho");
+        pizza.adicionaIngrediente("maionese");
+
+        int precoDaPizza = 20;
+
+        assertEquals(precoDaPizza, pizza.getPreco());
+    }
+
+    @Test
+    public void precoDaPizzaComCincoIngredientes() {
+        Pizza pizza = new Pizza();
+        pizza.adicionaIngrediente("queijo");
+        pizza.adicionaIngrediente("presunto");
+        pizza.adicionaIngrediente("alho");
+        pizza.adicionaIngrediente("maionese");
+        pizza.adicionaIngrediente("catchup");
+
+        int precoDaPizza = 20;
+
+        assertEquals(precoDaPizza, pizza.getPreco());
+    }
+
+    @Test
+    public void precoDaPizzaComSeisIngredientes() {
+        Pizza pizza = new Pizza();
+        pizza.adicionaIngrediente("queijo");
+        pizza.adicionaIngrediente("presunto");
+        pizza.adicionaIngrediente("alho");
+        pizza.adicionaIngrediente("maionese");
+        pizza.adicionaIngrediente("catchup");
+        pizza.adicionaIngrediente("tomate");
+
+        int precoDaPizza = 23;
+
+        assertEquals(precoDaPizza, pizza.getPreco());
+    }
+
+    @Test
+    public void contabilizaIngredientes() {
+        Pizza pizza1 = new Pizza();
+        pizza1.adicionaIngrediente("queijo");
+        pizza1.adicionaIngrediente("presunto");
+
+        Pizza pizza2 = new Pizza();
+        pizza2.adicionaIngrediente("queijo");
+        pizza2.adicionaIngrediente("alho");
+
+        assertEquals(1, Pizza.quantidadeDeUmIngredienteEmTodasAsPizzas("presunto"));
+        assertEquals(2, Pizza.quantidadeDeUmIngredienteEmTodasAsPizzas("queijo"));
+        assertEquals(1, Pizza.quantidadeDeUmIngredienteEmTodasAsPizzas("alho"));
+    }
 }
