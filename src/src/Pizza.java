05dc88601c271ab425382864da19a582e03ef23a
Joaom123 <joaomarcus12769@gmail.com>
2020-08-18 01:01:01 -0300

    getPreco e contabilizaIngrediente

--- src/src/Pizza.java
+++ src/src/Pizza.java
@@ -1,17 +1,20 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Pizza {
     public List<String> ingredientes;
+    public static HashMap<String, Integer> ingredientesDeTodasAsPizzas = new HashMap<>();
 
     public Pizza() {
-        ingredientes = new ArrayList<String>();
+        ingredientes = new ArrayList<>();
     }
 
     public void adicionaIngrediente(String ingrediente) {
         ingredientes.add(ingrediente);
+        contabilizaIngrediente(ingrediente);
     }
 
     public int getPreco() {
@@ -19,6 +22,20 @@ public class Pizza {
 
         if(quantidadeDeIngredientes < 3)
             return 15;
-        return ingredientes.size();
+
+        if(quantidadeDeIngredientes < 6)
+            return 20;
+
+        return 23;
+    }
+
+    private static void contabilizaIngrediente(String ingrediente) {
+        if(!ingredientesDeTodasAsPizzas.containsKey(ingrediente))
+            ingredientesDeTodasAsPizzas.put(ingrediente, 0);
+        ingredientesDeTodasAsPizzas.put(ingrediente, ingredientesDeTodasAsPizzas.get(ingrediente) + 1);
+    }
+
+    public static int quantidadeDeUmIngredienteEmTodasAsPizzas(String ingrediente) {
+        return Pizza.ingredientesDeTodasAsPizzas.get(ingrediente);
     }
 }
