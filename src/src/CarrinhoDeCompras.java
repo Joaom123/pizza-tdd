c46638ace0efe0250ff9988e26c7fec662a41d83
Joaom123 <joaomarcus12769@gmail.com>
2020-08-18 01:09:46 -0300

    Começando carrinho de compras

---
+++ src/src/CarrinhoDeCompras.java
@@ -0,0 +1,11 @@
+package src;
+
+import java.util.ArrayList;
+
+public class CarrinhoDeCompras {
+    private ArrayList<Pizza> pizzas;
+
+    public CarrinhoDeCompras() {
+        pizzas = new ArrayList<>();
+    }
+}
---
+++ src/test/CarrinhoDeComprasTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+import src.CarrinhoDeCompras;
+
+public class CarrinhoDeComprasTest {
+
+    @Test
+    public void carrinhoDeComprasInicializacao () {
+        CarrinhoDeCompras cc = new CarrinhoDeCompras();
+    }
+}
