551efe39b4802ccbe46113c9261da04ab46fca68
Joaom123 <joaomarcus12769@gmail.com>
2020-08-19 19:15:43 -0300

    CarrinhoDeCompras adicionaPizza e testes

--- src/src/CarrinhoDeCompras.java
+++ src/src/CarrinhoDeCompras.java
@@ -3,9 +3,14 @@ package src;
 import java.util.ArrayList;
 
 public class CarrinhoDeCompras {
-    private ArrayList<Pizza> pizzas;
+    public ArrayList<Pizza> pizzas;
 
     public CarrinhoDeCompras() {
         pizzas = new ArrayList<>();
     }
+
+    public void adicionaPizza(Pizza pizza) {
+        pizzas.add(pizza);
+    }
+
 }
--- src/test/CarrinhoDeComprasTest.java
+++ src/test/CarrinhoDeComprasTest.java
@@ -1,12 +1,43 @@
 package test;
 
-import org.junit.Test;
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
 import src.CarrinhoDeCompras;
+import src.Pizza;
 
 public class CarrinhoDeComprasTest {
 
     @Test
     public void carrinhoDeComprasInicializacao () {
         CarrinhoDeCompras cc = new CarrinhoDeCompras();
+    }
+
+    @Test
+    public void adicionaUmaPizza() {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+        Pizza pizza1 = new Pizza();
+
+        pizza1.adicionaIngrediente("queijo");
+        carrinhoDeCompras.adicionaPizza(pizza1);
+
+        assertEquals(pizza1, carrinhoDeCompras.pizzas.get(0));
+    }
+
+    @Test
+    public void adicionaDuasPizzas() {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+        Pizza pizza1 = new Pizza();
+        Pizza pizza2 = new Pizza();
+
+        pizza1.adicionaIngrediente("queijo");
+
+        pizza2.adicionaIngrediente("queijo");
+        pizza2.adicionaIngrediente("presunto");
+
+        carrinhoDeCompras.adicionaPizza(pizza1);
+        carrinhoDeCompras.adicionaPizza(pizza2);
+
+        assertEquals(pizza1, carrinhoDeCompras.pizzas.get(0));
+        assertEquals(pizza2, carrinhoDeCompras.pizzas.get(1));
     }
 }
